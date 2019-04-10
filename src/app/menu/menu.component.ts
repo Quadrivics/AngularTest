08e14ced21d75498e15fec0f055f6b3acc3db453
sbosman <s.bosman@afterpay.nl>
2019-04-10 16:13:24 +0200

    Angular Test Start of project with possible hibernate integration

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  public LOGO = require('./website-logo-wit.png');
   constructor() { }
 
   ngOnInit() {
